4322981f96179dda9355849d62e311bc9c3b8fb8
Joe Kutner <jpkutner@gmail.com>
2016-10-11 09:21:03 -0500

    Updated jce test

--- src/main/java/JCE.java
+++ src/main/java/JCE.java
@@ -8,7 +8,10 @@ import static javax.crypto.Cipher.DECRYPT_MODE;
 import static javax.crypto.Cipher.ENCRYPT_MODE;
 
 public class JCE {
-    private static final byte[] salt =
+
+    private static final char[] PASSWORD = "abcdefg".toCharArray();
+
+    private static final byte[] SALT =
             {
                     (byte)0x4D, (byte)0x9B, (byte)0xC6, (byte)0x53,
                     (byte)0x17, (byte)0xAF, (byte)0xE2, (byte)0x08
@@ -22,21 +25,30 @@ public class JCE {
             data += ":" + arg;
         }
 
-        SecretKeyFactory keyFac = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-        PBEKeySpec pbeKeySpec = new PBEKeySpec("abcdefg".toCharArray());
-        SecretKey myDesKey = keyFac.generateSecret(pbeKeySpec);
-        AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterations);
-
-        Cipher c = Cipher.getInstance("PBEWithMD5AndTripleDES");
-        c.init(ENCRYPT_MODE, myDesKey, paramSpec);
-
         System.out.println("Encrypting, \"" + data + "\"");
 
-        byte[] encryptedData = c.doFinal(data.getBytes());
+        byte[] encryptedData = encrypt(data);
         System.out.println("Encrypted: " + new String(encryptedData));
 
-        c.init(DECRYPT_MODE, myDesKey, paramSpec);
-        byte[] decipheredText = c.doFinal(encryptedData);
+        byte[] decipheredText = decrypt(encryptedData);
         System.out.println("Decrypted: " + new String(decipheredText));
+    }
+
+    private static byte[] encrypt(String property) throws Exception {
+        Cipher pbeCipher = getCipher(Cipher.ENCRYPT_MODE);
+        return pbeCipher.doFinal(property.getBytes());
+    }
+
+    private static byte[] decrypt(byte[] property) throws Exception {
+        Cipher pbeCipher = getCipher(Cipher.DECRYPT_MODE);
+        return pbeCipher.doFinal(property);
+    }
+
+    private static Cipher getCipher(int mode) throws Exception {
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+        SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
+        Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
+        pbeCipher.init(mode, key, new PBEParameterSpec(SALT, 20));
+        return pbeCipher;
     }
 }
